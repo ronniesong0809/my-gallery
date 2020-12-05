385210e9085c45295559779ccf4f875be0943040
Ronnie Song <ronniesong0809@gmail.com>
2020-12-05 00:33:09 -0800

    add tailwindcss

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title m-2">
+      <h1 class="text-blue-300 text-xl">Gallery</h1>
+      <h2 class="text-gray-500 text-5xl text-center mt-14">My Pictures</h2>
+    </div>
+  );
+};
+export default Title;
