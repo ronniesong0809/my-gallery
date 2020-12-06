87eef71e45a6288f07f9dd26899d22a8e6813e46
Ronnie Song <ronniesong0809@gmail.com>
2020-12-05 19:45:04 -0800

    add storage hook

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { projectStorage } from "../firebase/config";
+
+const useStorage = file => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    const storageRef = projectStorage.ref(file.name);
+
+    storageRef.put(file).on(
+      "state_changed",
+      snap => {
+        let precentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(precentage);
+      },
+      error => {
+        setError(error);
+      },
+      async () => {
+        const url = await storageRef.getDownloadUrl();
+        setUrl(url);
+      }
+    );
+  }, [file]);
+
+  return { progress, error, url };
+};
+
+export default useStorage;
