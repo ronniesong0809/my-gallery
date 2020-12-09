e36bc8684a55fafe552b7031d615c42cbbda36e6
Ronnie Song <ronniesong0809@gmail.com>
2020-12-09 03:59:51 -0800

    center oversized img in div

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -8,8 +8,8 @@ const ImageGrid = () => {
     <div className="img-grid mx-5 my-auto grid grid-cols-4 gap-10">
       {docs &&
         docs.map(doc => (
-          <div className="img-wrap overflow-hidden h-0 p-s-50 relative opacity-80" key={doc.id}>
-            <img className=" min-w-full min-h-full max-w-150 absolute top-0 left-0" src={doc.url} alt={doc.id} />
+          <div className="relative p-100" key={doc.id}>
+            <img className="w-full h-full object-cover absolute top-0 left-0" src={doc.url} alt={doc.id} />
           </div>
         ))}
     </div>
